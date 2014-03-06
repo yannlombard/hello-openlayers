cace32d4482c5ce42828abfc6b64a7549320f488
yannlombard <yann.lombard@dict.fr>
2014-03-06 11:27:49 +0100

    add grunt control

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,35 @@ module.exports = function (grunt) {
   // Define the configuration for all the tasks
   grunt.initConfig({
 
+      buildcontrol: {
+          options: {
+              dir: 'dist',
+              commit: true,
+              push: true,
+              message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+          },
+          pages: {
+              options: {
+                  remote: 'git@github.com:yannlombard/hello-openlayers.git',
+                  branch: 'gh-pages'
+              }
+          }
+          /*heroku: {
+           options: {
+           remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+           branch: 'master',
+           tag: pkg.version
+           }
+           },
+           local: {
+           options: {
+           remote: '../',
+           branch: 'build'
+           }
+           }*/
+      },
+
+
     // Project settings
     yeoman: {
       // configurable paths
@@ -392,7 +421,8 @@ module.exports = function (grunt) {
     'cssmin',
     'uglify',
     'rev',
-    'usemin'
+    'usemin',
+    'buildcontrol'
   ]);
 
   grunt.registerTask('default', [
